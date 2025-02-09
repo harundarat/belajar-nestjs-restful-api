f29d7319edd024acdbc893ecbc36ba782f62130d
Harun Al Rasyid <harundarat@gmail.com>
2025-02-09 14:03:30 +0700

    feat: add Swagger documentation setup for API

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
@@ -7,6 +8,14 @@ async function bootstrap() {
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats Example')
+    .setDescription('cats api documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
